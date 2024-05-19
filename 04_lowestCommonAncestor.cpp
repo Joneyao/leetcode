efc6d8671c250c20ee6d3ba242a528ad5a9d3214
yaojingwei <yaojingwei@xiaomi.com>
2024-05-19 09:15:59 +0800

    add lowestCommonAncestor.cpp code.
    Signed-off-by: yaojingwei <yaojingwei@xiaomi.com>

--- 04_lowestCommonAncestor.cpp
+++ 04_lowestCommonAncestor.cpp
@@ -8,56 +8,90 @@
  * };
  */
 
-#include <iostream>
-#include <set>
-#include <vector>
-
-using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x)
-        : val(x)
-        , left(NULL)
-        , right(NULL)
-    {
-    }
-};
-
 /**
 思路：
-情况1： AB两个节点在一边树上。
-先找到A节点，再找到B节点，AB之间的深度就是公共深度。
-
-情况2：AB不在一边树上。
-1. 先要找到AB的祖先。
-2. 计算A和B到祖先的距离。
-3. 返回最大的值即可。
-
-怎么找祖先？
-情况1：遍历所有节点，直到找到AB两个节点。
-情况2：记录便利A和B节点过程中的每个节点，有相同的节点，即为祖先。
-
-怎样遍历二叉树？
+1. 遍历所有节点，记录满足p和q公共祖先的节点的深度和node。
+2. 遍历结束后，直接返回记录的公共node即可。
 
 */
 
 class Solution {
 public:
+    int max_deepth = 0;
+    TreeNode* ancestor_node = NULL;
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
     {
-        vector<int> p_node_list;
-        vector<int> q_node_list;
+        preorder(root, p, q);
 
+        return ancestor_node;
+    }
+
+    void preorder(TreeNode* node, TreeNode* p, TreeNode* q)
+    {
+        if (node == NULL)
+            return;
+
+        TreeNode* ret = preorderFind(node, p, q);
+        if (ret) {
+            int deepth = getDeepth(ret);
+            if (deepth > max_deepth) {
+                ancestor_node = ret;
+            }
+        }
+
+        preorder(node->left, p, q);
+        preorder(node->right, p, q);
+    }
+
+    TreeNode* preorderFind(TreeNode* node, TreeNode* p, TreeNode* q)
+    {
+        if (node == NULL)
+            return NULL;
+
+        if (findNode(node, p) && findNode(node, q)) {
+            return node;
+        }
+
+        TreeNode* l_ret = preorderFind(node->left, p, q);
+        TreeNode* r_ret = preorderFind(node->right, p, q);
+
+        if (l_ret)
+            return l_ret;
+        if (r_ret)
+            return r_ret;
+        return NULL;
+    }
+
+    bool findNode(TreeNode* node, TreeNode* target_node)
+    {
+        if (!node) {
+            return false;
+        }
+
+        if (node->val == target_node->val)
+            return true;
+
+        int left_ret = findNode(node->left, target_node);
+        int right_ret = findNode(node->right, target_node);
+
+        return ((left_ret || right_ret) ? true : false);
+    }
+
+    // return deepth
+    int getDeepth(TreeNode* node)
+    {
+        if (node == NULL)
+            return 0;
+        else {
+            int m = 0;
+            int n = 0;
+            m = getDeepth(node->left);
+            n = getDeepth(node->right);
+            if (m > n)
+                return (m + 1);
+            else
+                return (n + 1);
+        }
     }
 };
-
-
-
-int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
-}
